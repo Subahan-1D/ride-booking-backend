cdf9bafce274e41ecfb0af17235a968487d3f6c2
subahan islam <subahanislam523@gmail.com>
2025-12-20 22:43:06 +0600

    added to Avoid Repetition of Try-Catch, use catchAsync

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,25 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes";
 
 import { UserService } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
+
 // import AppError from "../../errorHelpers/AppError";
 
-const createUser = async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    // throw new AppError(httpStatus.BAD_REQUEST, "fake error for testing ");
+// const createUser =  async (req: Request, res: Response, next: NextFunction) => {
+
+//     // throw new AppError(httpStatus.BAD_REQUEST, "fake error for testing ");
+//     const user = await UserService.createUser(req.body);
+//     res.status(httpStatus.CREATED).json({
+//       message: "User created successfully",
+//       data: user,
+//     });
+// };
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const user = await UserService.createUser(req.body);
     res.status(httpStatus.CREATED).json({
       message: "User created successfully",
       data: user,
     });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (err: any) {
-    // eslint-disable-next-line no-console
-    console.log(err);
-    next(err);
   }
-};
+);
+
+const getAllUsers = catchAsync(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async (req: Request, res: Response, next: NextFunction) => {
+    const users = await UserService.getAllUsers();
+    res.status(httpStatus.OK).json({
+      message: "All Users retrieved successfully",
+      data: users,
+    });
+  }
+);
 
 export const UserController = {
   createUser,
+  getAllUsers,
 };
