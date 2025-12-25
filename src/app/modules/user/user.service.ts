05a7426e09dc70a874efa32c0633d65e15b010ce
subahan islam <subahanislam523@gmail.com>
2025-12-25 11:50:48 +0600

    added email password based User registration API

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,11 +1,27 @@
-import { IUser } from "./user.interface";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import AppError from "../../errorHelpers/AppError";
+import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import httpStatus from "http-status-codes";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { name, email } = payload;
+  const {  email, ...rest } = payload;
+
+  const isUserExists = await User.findOne({ email });
+  if (isUserExists) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "User already exists with this email"
+    );
+  }
+  const authProvider: IAuthProvider = {
+    provider: "credentials",
+    providerId: email as string,
+  };
   const user = await User.create({
-    name,
     email,
+    auths : [authProvider],
+    ...rest,
   });
 
   return user;
