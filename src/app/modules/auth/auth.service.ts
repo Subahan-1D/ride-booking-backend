c59148aff7177e1acc83d8c15a0c122e60ce9482
subahan islam <subahanislam523@gmail.com>
2026-01-03 15:22:30 +0600

    added  Create Login API

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import AppError from "../../errorHelpers/AppError";
+import { IUser } from "../user/user.interface";
+import { User } from "../user/user.model";
+import bcryptjs from "bcryptjs";
+import httpStatus from "http-status-codes";
+const credentialsLogin = async (payload: Partial<IUser>) => {
+  const { email, password } = payload;
+
+  const isUserExists = await User.findOne({ email });
+  if (!isUserExists) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
+  }
+
+  const isPasswordMatched = await bcryptjs.compare(
+    password as string,
+    isUserExists.password as string
+  );
+  if (!isPasswordMatched) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Password is incorrect");
+  }
+
+  return {
+    email: isUserExists.email,
+  };
+};
+
+export const AuthServices = {
+  credentialsLogin,
+};
